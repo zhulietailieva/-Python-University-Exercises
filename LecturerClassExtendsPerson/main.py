05f72ba3b47857576a5b6c21569970172c55ffbf
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2022-11-24 15:32:08 +0200

    Added a new exercise.

---
+++ LecturerClassExtendsPerson/main.py
@@ -0,0 +1,57 @@
+class Person:
+    def __init__(self,firstName,lastName,age,nationality):
+        self.firstName=firstName
+        self.lastName=lastName
+        self.age=age
+        self.nationality=nationality
+
+    def printInfo(self):
+        print("I am",self.firstName,self.lastName,"and I am",self.nationality)
+
+person1=Person("Ivan", "Ivanov", 22, "Bulgarian")
+person2=Person("Petar","Petrov",46,"Canadian")
+# person1.printInfo()
+# person2.printInfo()
+
+class Student(Person):
+    def __init__(self, firstName, lastName, age, nationality,university,yearOfGraduation):
+       super().__init__(firstName,lastName,age,nationality)
+       self.university=university
+       self.yearOfGraduation=yearOfGraduation
+
+    def printInfo(self):
+        print("I am", self.firstName, self.lastName, "and I am a",self.nationality,"student from",self.university,"and I will graduate in", str(self.yearOfGraduation))
+
+student1=Student("Maria", "Petrova", 22, "Bulgarian","TU Sofia",2028)
+#student1.printInfo()
+
+class Lecturer(Person):
+    def __init__(self, firstName, lastName, age, nationality,university,experience):
+       super().__init__(firstName,lastName,age,nationality)
+       self.university=university
+       self.experience=experience
+
+    def printInfo(self):
+        print("I am", self.firstName, self.lastName, "and I am a", self.nationality, "lecturer from", self.university,
+              "and I have", str(self.experience),"years of experience")
+
+lecturer1=Lecturer("Doan", "Pavlov", 43, "Indian","TU Sofia",6)
+# lecturer1.printInfo()
+def findMostExperienced(lecturers):
+    mostexperiencedlecturer = Lecturer("", "", -1, "", "", -1)
+    for x in lecturers:
+        if (x.experience > mostexperiencedlecturer.experience):
+            mostexperiencedlecturer = x
+    print("Most experienced lecturer:")
+    mostexperiencedlecturer.printInfo()
+
+lecturers=[]
+for x in range(5):
+    print('Lecturer ',x+1)
+    newLecturer=Lecturer(input("First name: "),input("Last name: "),
+                         int(input("Age: ")),input("Nationality: "),
+                         input("University: "),
+                         int(input("Years of experience: ")))
+    lecturers.append(newLecturer)
+
+findMostExperienced(lecturers)
